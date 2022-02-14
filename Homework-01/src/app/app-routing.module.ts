93a48728a6b448a098ab03b41a8535bb068ec040
richardljohn <richardljohn@gmail.com>
2022-02-14 18:33:48 -0500

    New Page

--- Homework-01/src/app/app-routing.module.ts
+++ Homework-01/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'items',
+    loadChildren: () => import('./items/items.module').then( m => m.ItemsPageModule)
+  },
 ];
 
 @NgModule({
